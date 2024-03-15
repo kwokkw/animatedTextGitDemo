eb96860656ac15e19e54a74eaec33aff6d24f3eb
Kwok <kwokkw@users.noreply.github.com>
2024-03-14 14:48:42 -1000

    commit demo

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ function randomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r} ${g} ${b})`;
 }
 
 // To change each letter independently
 const letters = document.querySelectorAll(".letter");
-const intervalId = setInterval(() => {
+const intervalId = setInterval(function () {
   for (let letter of letters) {
-    letter.computedStyleMap.color = randomRGB();
+    letter.style.color = randomRGB();
   }
 }, 1000);
